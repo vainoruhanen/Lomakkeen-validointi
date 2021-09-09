90c74006178a7202d708efe81cf6576d5d4755d8
Väinö Ruhanen <vaino.ruhanen@gmail.com>
2021-09-09 10:17:48 +0300

    toteutin validoinnin javascriptillä

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 
-function checkIfEmpty(){
+function validate(){
     var onTyhjia = false;
     let kayttajaid = document.forms["formi"]["kayttajaID"].value;
-    if(kayttajaid== ""){
+    if(kayttajaid.length < 6){
         onTyhjia = true;
     }
 
     let salasana = document.forms["formi"]["salasana"].value;
     if(salasana== ""){
-        onTyhjia = true;
+        onTyhjia = true; 
     }
     let nimi = document.forms["formi"]["nimi"].value;
     if(nimi== ""){
@@ -21,18 +21,45 @@ function checkIfEmpty(){
         onTyhjia = true;
     }
 
-    let postinumero = document.forms["formi"]["postinumero"].value;
-    if(postinumero== ""){
+    var postinumero = document.forms["formi"]["postinumero"].value;
+    if(postinumero==""){
         onTyhjia = true;
     }
+    if (isNaN(postinumero)){
+        onTyhjia = true;
+    }
+    if (postinumero.length != 5){
+        onTyhjia = true;
+    }
+   
 
     let sahkoposti = document.forms["formi"]["sahkoposti"].value;
     if(sahkoposti== ""){
         onTyhjia = true;
     }
+    if(sahkoposti.length < 3){
+        onTyhjia = true;
+    }
+    if (!(sahkoposti.indexOf('@') > -1))
+    {
+      onTyhjia = true;
+    }
+
+    var suomi = document.getElementById("suomi");
+    var muu = document.getElementById("muu");
+    if(!(muu.checked || suomi.checked)){
+        onTyhjia = true;
+    }
+
+    var mies = document.getElementById("mies");
+    var nainen = document.getElementById("nainen");
+    if(!(mies.checked || nainen.checked)){
+        onTyhjia = true;
+    }
+
 
     if(onTyhjia == true){
-        alert("kentät ei saa olla tyhjiä");
+        alert("Tarkista kentät!");
     }
 
 }
