97d472f78a69f08bf1dfc7659200be41077131c1
Väinö Ruhanen <vaino.ruhanen@gmail.com>
2021-09-24 12:31:42 +0300

    lisäsin yksityiskohtaisemman virhetulostuksen

--- script.js
+++ script.js
@@ -1,65 +1,110 @@
 
 
+var kayttajaIDtooShort = "\nKäyttäjä ID minimipituus 6";
+var salasanaTyhja = "\nSalasana ei saa olla tyhjä";
+var nimiTyhja = "\nNimi ei saa olla tyhjä";
+var osoiteVaara = "\nOsoite ei saa olla tyhjä";
+var postinumeroVaara = "\nPostinumeron pitää olla 5 numeroa";
+var sahkopostiVaara = "\nLaita oikea sähköposti osoite";
+var maaTyhja = "\nValitse maa";
+var sukupuoliTyhja = "\nValitse sukupuoli";
+var valitseKieli = "\nValitse kieli";
+
+var virhetulostus = "Tarkista seuraavat virheet";
+
+
+
+
+
+
+
 function validate(){
     var onTyhjia = false;
     let kayttajaid = document.forms["formi"]["kayttajaID"].value;
     if(kayttajaid.length < 6){
         onTyhjia = true;
+        virhetulostus += kayttajaIDtooShort;
     }
 
     let salasana = document.forms["formi"]["salasana"].value;
     if(salasana== ""){
         onTyhjia = true; 
+        virhetulostus += salasanaTyhja;
     }
     let nimi = document.forms["formi"]["nimi"].value;
     if(nimi== ""){
         onTyhjia = true;
+        virhetulostus += nimiTyhja;
     }
 
     let osoite = document.forms["formi"]["osoite"].value;
     if(osoite== ""){
         onTyhjia = true;
+        virhetulostus += osoiteVaara;
     }
+
+
+
+    var maaForm = document.getElementById("maa");
+    var valittu = maaForm.options[maaForm.selectedIndex].value;
+
+    if(valittu != "suomi" || valittu != "ruotsi" || valittu != "muu"){
+       virhetulostus += maaTyhja;
+    }
+
+
 
     var postinumero = document.forms["formi"]["postinumero"].value;
     if(postinumero==""){
         onTyhjia = true;
+        virhetulostus += postinumeroVaara;
     }
-    if (isNaN(postinumero)){
+    else if (isNaN(postinumero)){
         onTyhjia = true;
-    }
-    if (postinumero.length != 5){
+        virhetulostus += postinumeroVaara;
+    } 
+    else if (postinumero.length != 5){
         onTyhjia = true;
+        virhetulostus += postinumeroVaara;
     }
    
 
     let sahkoposti = document.forms["formi"]["sahkoposti"].value;
     if(sahkoposti== ""){
         onTyhjia = true;
+        virhetulostus += sahkopostiVaara;
     }
-    if(sahkoposti.length < 3){
+    else if(sahkoposti.length < 3){
         onTyhjia = true;
+        virhetulostus += sahkopostiVaara;
     }
-    if (!(sahkoposti.indexOf('@') > -1))
-    {
-      onTyhjia = true;
+   else if (!(sahkoposti.indexOf('@') > -1))
+    { 
+         onTyhjia = true;     
+        virhetulostus += sahkopostiVaara;
     }
 
     var suomi = document.getElementById("suomi");
     var muu = document.getElementById("muu");
     if(!(muu.checked || suomi.checked)){
         onTyhjia = true;
+        virhetulostus += valitseKieli;
     }
 
     var mies = document.getElementById("mies");
     var nainen = document.getElementById("nainen");
     if(!(mies.checked || nainen.checked)){
         onTyhjia = true;
+        virhetulostus += sukupuoliTyhja;
     }
 
 
+
+
+
     if(onTyhjia == true){
-        alert("Tarkista kentät!");
+        alert(virhetulostus);
+        virhetulostus = "Tarkista seuraavat virheet";
     }
 
 }
